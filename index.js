f8c62b3444a25d760030d0aaa96739615ee5cf78
Colin Tong <tongcolin@live.ca>
2022-04-08 20:03:48 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,131 @@
+document.addEventListener("DOMContentLoaded", main);
+
+const DEBOUNCE_DELAY = 200;
+
+const HACK_NAME_COLUMN_INDEX = 0;
+const AUTHOR_NAME_COLUMN_INDEX = 1;
+const HACK_DATE_COLUMN_INDEX = 2;
+
+/**
+ * @typedef {Object} HackTableRowContent
+ * @property {string} hackName
+ * @property {string} authorName
+ * @property {string} hackDate
+ * @property {HTMLTableRowElement} tableRow
+ */
+
+/**
+ * @typedef {"hackName" | "authorName" | "hackDate"} SearchKey
+ */
+
+function main() {
+  const myTable = document.getElementById("myTable");
+
+  /** @type {HackTableRowContent[]} */
+  const tableRowContents = Array.from(myTable.getElementsByTagName("tr")).slice(1).map((tableRow) => {
+    const columns = tableRow.getElementsByTagName("td");
+    return {
+      hackName: columns[HACK_NAME_COLUMN_INDEX].innerText.toUpperCase(),
+      authorName: columns[AUTHOR_NAME_COLUMN_INDEX].innerText.toUpperCase(),
+      hackDate: columns[HACK_DATE_COLUMN_INDEX].innerText.toUpperCase(),
+      tableRow,
+    }
+  });
+
+  const hackNamesInput = document.getElementById("hackNamesInput");
+  setHackNamesFilterHandler(hackNamesInput, tableRowContents);
+
+  const authorNamesInput = document.getElementById("authorNamesInput");
+  setAuthorNamesFilterHandler(authorNamesInput, tableRowContents);
+
+  const hackDatesInput = document.getElementById("hackDatesInput");
+  setHackDatesFilterHandler(hackDatesInput, tableRowContents);
+}
+
+/**
+ * @param {HTMLInputElement} hackNamesInput
+ * @param {HackTableRowContent[]} tableRowContents
+ */
+function setHackNamesFilterHandler(hackNamesInput, tableRowContents) {
+  hackNamesInput.addEventListener("keyup", debounce((keyUpEvent) => {
+    const searchString = keyUpEvent.target.value.toUpperCase();
+    filterRows(searchString, "hackName", tableRowContents);
+  }), DEBOUNCE_DELAY);
+}
+
+/**
+ * @param {HTMLInputElement} authorNamesInput
+ * @param {HackTableRowContent[]} tableRowContents
+ */
+function setAuthorNamesFilterHandler(authorNamesInput, tableRowContents) {
+  authorNamesInput.addEventListener("keyup", debounce((keyUpEvent) => {
+    const searchString = keyUpEvent.target.value.toUpperCase();
+    filterRows(searchString, "authorName", tableRowContents);
+  }), DEBOUNCE_DELAY);
+}
+
+/**
+ * @param {HTMLInputElement} hackDatesInput
+ * @param {HackTableRowContent[]} tableRowContents
+ */
+function setHackDatesFilterHandler(hackDatesInput, tableRowContents) {
+  hackDatesInput.addEventListener("keyup", debounce((keyUpEvent) => {
+    const searchString = keyUpEvent.target.value.toUpperCase();
+    filterRows(searchString, "hackDate", tableRowContents);
+  }), DEBOUNCE_DELAY);
+}
+
+/**
+ * @param {string} searchString
+ * @param {SearchKey} keyName
+ * @param {HackTableRowContent[]} tableRowContents
+ */
+ function filterRows(searchString, keyName, tableRowContents) {
+  if (searchString === "") {
+    tableRowContents.forEach((tableRowContent) => showTableRow(tableRowContent.tableRow));
+  } else {
+    tableRowContents.forEach((tableRowContent) => {
+      const tableRow = tableRowContent.tableRow;
+      if (tableRowContent[keyName].includes(searchString)) {
+        showTableRow(tableRow);
+      } else {
+        hideTableRow(tableRow);
+      }
+    })
+  }
+}
+
+/**
+ * @param {HTMLTableRowElement} tableRow
+ */
+function showTableRow(tableRow) {
+  tableRow.style.display = "table-row";
+}
+
+/**
+ * @param {HTMLTableRowElement} tableRow
+ */
+function hideTableRow(tableRow) {
+  tableRow.style.display = "none";
+}
+
+/**
+ * @function debounce
+ * @param {Function} callback function to invoke after calls have been debounced
+ * @param {number} delay number of milliseconds of debounce delay
+ * 
+ * @returns {Function} modified function with debounce logic
+ */
+function debounce(callback, delay) {
+  let timeout;
+
+  return function debouncedFunction(...args) {
+    const delayedFunction = () => {
+      clearTimeout(timeout);
+      callback(...args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(delayedFunction, delay);
+  };
+}
